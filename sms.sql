b1f42d464d216961b047814d1757573e30384c84
黄宇辉 <43493852+YUbuntu0109@users.noreply.github.com>
2019-05-30 12:57:11 +0800

    :sunny: update the database file ~

---
+++ sms.sql
@@ -0,0 +1,149 @@
+CREATE DATABASE  IF NOT EXISTS `sms` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `sms`;
+-- MySQL dump 10.13  Distrib 8.0.11, for Win64 (x86_64)
+--
+-- Host: localhost    Database: sms
+-- ------------------------------------------------------
+-- Server version	8.0.11
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `classinfo`
+--
+
+DROP TABLE IF EXISTS `classinfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `classinfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `introduce` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8 COMMENT='班级信息表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `classinfo`
+--
+
+LOCK TABLES `classinfo` WRITE;
+/*!40000 ALTER TABLE `classinfo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `classinfo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_admin`
+--
+
+DROP TABLE IF EXISTS `user_admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user_admin` (
+  `status` int(2) DEFAULT NULL,
+  `id` varchar(20) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `status_UNIQUE` (`status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='管理员信息表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_admin`
+--
+
+LOCK TABLES `user_admin` WRITE;
+/*!40000 ALTER TABLE `user_admin` DISABLE KEYS */;
+INSERT INTO `user_admin` VALUES (1,'001','黄宇辉','Test0109');
+/*!40000 ALTER TABLE `user_admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_student`
+--
+
+DROP TABLE IF EXISTS `user_student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user_student` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `classID` int(20) NOT NULL,
+  `sno` varchar(20) NOT NULL,
+  `name` varchar(10) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `sex` varchar(2) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `mobile` varchar(12) NOT NULL,
+  `photo` mediumblob,
+  PRIMARY KEY (`id`,`classID`,`sno`),
+  UNIQUE KEY `sno_UNIQUE` (`sno`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `class_student_key_idx` (`classID`),
+  CONSTRAINT `class_student_key` FOREIGN KEY (`classID`) REFERENCES `classinfo` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='学生信息表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_student`
+--
+
+LOCK TABLES `user_student` WRITE;
+/*!40000 ALTER TABLE `user_student` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_student` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_teacher`
+--
+
+DROP TABLE IF EXISTS `user_teacher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user_teacher` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `classID` int(20) NOT NULL,
+  `tno` varchar(20) NOT NULL,
+  `name` varchar(10) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `sex` varchar(2) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `mobile` varchar(12) NOT NULL,
+  `photo` mediumblob,
+  PRIMARY KEY (`id`,`classID`,`tno`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `tno_UNIQUE` (`tno`),
+  KEY `class_teacher_key_idx` (`classID`),
+  CONSTRAINT `class_teacher_key` FOREIGN KEY (`classID`) REFERENCES `classinfo` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='教师信息表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_teacher`
+--
+
+LOCK TABLES `user_teacher` WRITE;
+/*!40000 ALTER TABLE `user_teacher` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_teacher` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-05-30 12:29:32
